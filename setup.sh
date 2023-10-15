de6b7d4a8d981635d05a6e0d04e0e961748b7a75
EstebanCabreraArbizu <u202014600@upc.edu.pe>
2023-10-15 17:56:05 -0500

    copia del repositorio del profesor canaval

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+export CLASSPATH=.:~/Apps/antlr4-4.13.1-complete.jar:$CLASSPATH
+alias grun='java org.antlr.v4.gui.TestRig'
+
+export ANTLR4HOME=/home/lmcs/Apps/antlr4-cpp/run
+export ANTLR4I=/home/lmcs/Apps/antlr4-cpp/run/usr/local/include/antlr4-runtime
+export ANTLR4L=/home/lmcs/Apps/antlr4-cpp/run/usr/local/lib/
+
